9c5b5d01b94320ee37cfe86efeb5a70e848d19c6
Oliver Kraemer <oliverkra@gmail.com>
2015-03-23 00:16:20 -0300

    Read assets from local directory and cache this in memcached

--- README.md
+++ README.md
@@ -1,13 +1,27 @@
-CDN
-===
-
+###About
 A simple CDN server with `mongodb` for read content and `memcached` for fast cache
 
-## ENV Params
- - PORT: (Usage: PORT=80) Define listen port server
- - CONCURRENCE_PROCESS: (Usage: CONCURRENCE_PROCESS=4) Define count of process running the server
+---
 
-## Use
+###Dependencies
+- [nodejs](https://nodejs.org/)
+- [npm](https://www.npmjs.com/)*
+- [memcached](http://memcached.org/)**
+
+---
+
+###Setup
+1. Install nodejs
+2. Install npm
+3. Install memcached
+4. Install NPM dependencies ```npm install```
+5. Change directory to ```cd ./tools```
+6. Run command ```./download-assets-bower.sh```. This command will download some bower packages.
+7. Return to root project directory ```cd ..```
+
+---
+
+###Use
 
 ```shell
 	node index.js
@@ -19,13 +33,15 @@ or with specific port
 	PORT=80 node index.js
 ```
 
-running with single process
+or running with single process
 
 ```shell
 	CONCURRENCE_PROCESS=1 PORT=80 node index.js
 ```
 
-## Tests
+---
+
+###Tests
 
 ```shell
 	npm test
@@ -37,12 +53,27 @@ or
 	gulp watch
 ```
 
+---
+
 ## Stress test
 
 ```shell
 apt-get install siege
-siege -c100 -t1M http://localhost:9999/
+siege -c100 -t1M http://localhost:9999/vendors/jquery/2.1.3/jquery.min.js
 ```
+
+or with ab (apache)
+
+```shell
+ab -c 100 -t 60 http://localhost:9999/vendors/jquery/2.1.3/jquery.min.js
+```
+
+---
 
 ## Release History
 * 0.1.0 Initial release
+* 0.1.1 Reading packages from directory and cache in memorycache
+
+---
+
+code with :heart: and keep :muscle: ...
--- server.js
+++ server.js
@@ -4,34 +4,69 @@ var express = require('express'),
     memcache = require('memcache'),
     util = require('util'),
     cluster = require('cluster'),
+    fs = require('fs'),
     app = module.exports.app = exports.app = express(),
     appCache = new memcache.Client(11211, '127.0.0.1');
 
+app.disable('x-powered-by');
 app.use(logger('dev'));
 app.use(compression({
     level: 9
 }));
+app.use(function (req, res, next) {
+    req.on('end', function () {
+        var organization = req.params.organization,
+            version = req.params.version,
+            file = req.params.file;
+
+        switch (res.statusCode) {
+            case 200:
+                util.debug(util.format('hit count +1 %s/%s/%s', organization, version, file));
+                break;
+            case 304:
+                util.debug(util.format('hit cache count +1 %s/%s/%s', organization, version, file));
+                break;
+            case 404:
+                util.debug(util.format('hit error count +1 %s/%s/%s', organization, version, file));
+                break;
+        }
+    });
+    next();
+});
 
 // Connect with memcached server
 appCache.connect();
 
 // Routes
-const ROUTE_VENDORS = '/vendors/:organization([\\w_-]+)/:version(\\d\.\\d\.\\d)/:file([\\w_-]+\.[a-z]{2,3})';
+const ROUTE_VENDORS = '/vendors/:organization([\\w_-]+)/:version(\\d\.\\d\.\\d)/:file([\\w\\_\\-\\.]+\.[a-z]{2,3})';
 app.get(ROUTE_VENDORS, function (req, res) {
     var organization = req.params.organization,
         version = req.params.version,
         file = req.params.file,
-        fullPathFile = util.format('/vendors/%s/%s/%s', organization, version, file);
+        fullPathFile = util.format('/%s/%s/%s', organization, version, file),
+        vendorFullPathFile = '/vendor' + fullPathFile,
+        diskFullPathFile = 'tools/packages' + fullPathFile;
 
-    appCache.get(fullPathFile, function (err, result) {
+    appCache.get(vendorFullPathFile, function (err, result) {
         if (err || result === null) {
-            res.send("alert(\'simple javascript example\');\n");
+            fs.exists(diskFullPathFile, function (exists) {
+                if (exists) {
+                    fs.readFile(diskFullPathFile, function (err, data) {
+                        if (!err) {
+                            res.set('Content-Type', 'text/javascript');
+                            res.send(data);
+                            appCache.set(vendorFullPathFile, data);
+                        } else {
+                            res.sendStatus(404);
+                        }
+                    });
+                } else {
+                    res.sendStatus(404);
+                }
+            });
         } else {
-            res.send("alert(\'simple javascript example\');\n");
+            res.set('Content-Type', 'text/javascript');
+            res.send(result);
         }
     });
-});
-
-app.get('/', function (req, res) {
-    res.send('aasdasd');
 });
